d015bff313f6a174164f9b287a876675c063091b
probro27 <prabhavkhera@hotmail.com>
2021-09-18 02:04:37 -0400

    First commit

---
+++ src/components/Homepage/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-5 col-md-12">
+                    Aliqua ullamco elit ea ut elit nulla proident cupidatat eu ipsum. Fugiat non anim aute tempor voluptate cillum in consequat mollit. Ut nulla Lorem laboris aliqua pariatur ullamco reprehenderit id commodo consequat id.
+                </div>
+                <div className="col-lg-5 col-md-12">
+                    <img src="http://placehold.it/" alt="Placeholder image"></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/components/Homepage/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./styles.css";
+function Navbar() {
+  return (
+    <div className="navbar navbar-expand-md navbar-sticky-top bg-faded navbar-center">
+      <div className="container">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navbar-collapse navbar-collapse" id="navbarResponsive">
+          <a className="navbar-brand" href="#">
+            Price Matcher
+          </a>
+          <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+          <button className="btn btn-primary">Try It Out!</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
