51d1d1385ebf8baf313551fc997737c3d6c3b7ba
probro27 <prabhavkhera@hotmail.com>
2021-09-18 03:22:28 -0400

    Banner done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './components/Homepage/Banner';
 import Navbar from './components/Homepage/Navbar';
 
--- src/components/Homepage/Banner.js
+++ src/components/Homepage/Banner.js
@@ -2,13 +2,20 @@ import React from 'react'
 
 function Banner() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-5 col-md-12">
+        <div className="container banner">
+            <div className="row flex">
+                <div className="col-lg-6 col-md-12">
+                    <div className="heading">
+                        <h2 className="display-6">Paying too much?</h2>
+                    </div>
                     Aliqua ullamco elit ea ut elit nulla proident cupidatat eu ipsum. Fugiat non anim aute tempor voluptate cillum in consequat mollit. Ut nulla Lorem laboris aliqua pariatur ullamco reprehenderit id commodo consequat id.
+                    <div className="buttons">
+                        <button type="button" className="btn btn-primary">Get Started!</button>
+                        <button type="button" className="btn btn-outline-info">Learn More</button>
+                    </div>
                 </div>
                 <div className="col-lg-5 col-md-12">
-                    <img src="http://placehold.it/" alt="Placeholder image"></img>
+                    <img src="./Banner.png" alt="Placeholder image" width="300" height="230"></img>
                 </div>
             </div>
         </div>
